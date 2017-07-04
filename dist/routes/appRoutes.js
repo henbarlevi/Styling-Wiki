e36abaaa8b941556f78ff8bd72ab0adb67a359a9
henbarlevi <hen101020@gmail.com>
2017-07-04 08:13:05 +0300

    minor

--- dist/routes/appRoutes.js
+++ dist/routes/appRoutes.js
@@ -6,3 +6,16 @@ router.get('/', (req, res) => {
     res.send('welcome to server api');
 });
 exports.default = router;
+//-------------------------------------SNIPPETS-------------------------
+//CONVERTING NODE FS callback to REACTIVE
+// fs.readdir('./dist/routes',(err,items)=>{
+//     if(err){
+//         console.log(err);
+//     }
+//     else{
+//         console.log(items);
+//     }
+// })
+// //converting node callback function to reactive version:
+// const readdir$ = Rx.Observable.bindNodeCallback(fs.readdir); //save it as a function
+// readdir$('./dist/routes').subscribe(items=>{console.log(items)}); 
--- src/routes/appRoutes.ts
+++ src/routes/appRoutes.ts
@@ -1,4 +1,6 @@
 import * as express from 'express';
+import * as fs from 'fs';
+import * as Rx from 'rxjs';
 const router :express.Router = express.Router();
 
 
@@ -10,4 +12,21 @@ router.get('/',(req: express.Request, res: express.Response) => {
 
 
 
+
 export default router  ;
+
+
+//-------------------------------------SNIPPETS-------------------------
+
+//CONVERTING NODE FS callback to REACTIVE
+// fs.readdir('./dist/routes',(err,items)=>{
+//     if(err){
+//         console.log(err);
+//     }
+//     else{
+//         console.log(items);
+//     }
+// })
+// //converting node callback function to reactive version:
+// const readdir$ = Rx.Observable.bindNodeCallback(fs.readdir); //save it as a function
+// readdir$('./dist/routes').subscribe(items=>{console.log(items)}); 
